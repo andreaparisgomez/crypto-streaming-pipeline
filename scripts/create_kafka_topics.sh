7f179a312ad968996f1e39e70c4bea0d2fdc03b9
Andrea Paris <andreaparis.es@gmail.com>
2026-06-04 16:06:00 +0100

    Add 'if-not-exists' flag to topic creation commands

--- scripts/create_kafka_topics.sh
+++ scripts/create_kafka_topics.sh
@@ -3,6 +3,7 @@
 docker exec broker /opt/kafka/bin/kafka-topics.sh \
   --bootstrap-server localhost:9092 \
   --create \
+  --if-not-exists \
   --topic crypto_prices \
   --partitions 1 \
   --replication-factor 1
@@ -10,6 +11,7 @@ docker exec broker /opt/kafka/bin/kafka-topics.sh \
 docker exec broker /opt/kafka/bin/kafka-topics.sh \
   --bootstrap-server localhost:9092 \
   --create \
+  --if-not-exists \
   --topic crypto_metrics \
   --partitions 1 \
   --replication-factor 1
